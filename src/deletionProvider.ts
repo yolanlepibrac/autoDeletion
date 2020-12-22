10c380723b94e72aa0edd96f2410d2c6a645a20d
yolanpibrac <yolan.pibrac@gmail.com>
2020-12-22 13:18:39 +0100

    feature(deletionProvider): create snippet deletion on completionItem

--- src/deletionProvider.ts
+++ src/deletionProvider.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import { DeletionItem } from './deletionItem';
 
 let currentSuggestion = undefined;
 
@@ -6,11 +7,19 @@ export class DeletionProvider implements vscode.CompletionItemProvider {
    
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken):vscode.CompletionItem[] | vscode.CompletionList {
         const line = document.lineAt(position.line);
-        return [
-            new vscode.CompletionItem('ya', vscode.CompletionItemKind.Snippet),
-            new vscode.CompletionItem('yo', vscode.CompletionItemKind.Snippet),
-            new vscode.CompletionItem('hello', vscode.CompletionItemKind.Snippet),
-        ];
+        const cursorLine = position.line;
+        const cursorChar = position.character;
+        const sentence  = line.text;
+        const propositions  = new Array(6).fill(undefined).map((_undefined, index) => {
+            return sentence.substring(0, sentence.length - index);
+        }).reverse();
+        const deletionItems = propositions.map((text, index) => {
+            const item  =  new vscode.CompletionItem(text, vscode.CompletionItemKind.Snippet);
+            item.insertText = text;
+            item.additionalTextEdits = [vscode.TextEdit.delete(new vscode.Range(cursorLine,0,cursorLine,cursorChar))];
+            return item;
+        });
+        return deletionItems;
     }
 
     resolveCompletionItem(item: vscode.CompletionItem, _token: vscode.CancellationToken): vscode.ProviderResult<vscode.CompletionItem> {
