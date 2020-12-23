0fdbb8105aa963e9c24a0b750a56c93a60f277ba
yolanpibrac <yolan.pibrac@gmail.com>
2020-12-24 00:44:55 +0100

    bugfix(app): lint

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,26 @@
 import * as vscode from 'vscode';
 import { DeletionProvider } from './deletionProvider';
 
+const selector : vscode.DocumentSelector = [{ pattern: '**' }];
+const provider = new DeletionProvider();
+
 export function activate(context: vscode.ExtensionContext) {
-
-	var selector : vscode.DocumentSelector = [{
-        pattern: '**'
-	}];
-	const provider = new DeletionProvider();
-	const autoCompletionProvider = vscode.languages.registerCompletionItemProvider(
-		selector,
-		provider,
-		'.'
-	);
-
-	let autoDeletion = vscode.commands.registerCommand('autoDeletion.autoDelete', () => autoDelete());
+	const autoCompletionProvider = vscode.languages.registerCompletionItemProvider(selector, provider, '.');
+	const autoDeletion = vscode.commands.registerCommand('autoDeletion.autoDelete', () => autoDelete());
 	context.subscriptions.push(autoCompletionProvider, autoDeletion);
 }
 
 async function  autoDelete() {
-	
 	const editor = vscode.window.activeTextEditor;
 	if(!editor){
 		return; 
 	}
-
 	const cursorLine = editor.selection.active.line;
 	const cursorPos = editor.selection.active.character;
 	editor.edit(editBuilder => {
 		editBuilder.replace(new vscode.Range(cursorLine,cursorPos,cursorLine,cursorPos), ".");
 	});
 	vscode.commands.executeCommand('editor.action.triggerSuggest');
-	
-	
 }
 
 export function deactivate() {}
